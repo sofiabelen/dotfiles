3d7a26c049d19e7fdb049b77f3b7788ed896ebfe
Sofia B. Lopez <sofiabelenlopez00@gmail.com>
2019-06-18 14:05:27 +0300

    Add shortcut for Latex/resume

--- .config/qutebrowser/shortcuts.py
+++ .config/qutebrowser/shortcuts.py
@@ -1,6 +1,7 @@
 config.bind(";h", "set downloads.location.directory ~/ ;; hint links download")
 config.bind(";d", "set downloads.location.directory ~/Documents ;; hint links download")
 config.bind(";l", "set downloads.location.directory ~/Documents/Latex ;; hint links download")
+config.bind(";re", "set downloads.location.directory ~/Documents/Latex/resume ;; hint links download")
 config.bind(";w", "set downloads.location.directory ~/Pictures/Wallpapers ;; hint links download")
 config.bind(";p", "set downloads.location.directory ~/Pictures ;; hint links download")
 config.bind(";s", "set downloads.location.directory ~/Pictures/PrivateScreenshots ;; hint links download")
