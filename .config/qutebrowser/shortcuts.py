c52ddc98a6845f249661c06b03b334f26e7c98d2
Sofia B. Lopez <sofiabelenlopez00@gmail.com>
2019-10-13 16:12:10 +0300

    Update shortcuts

--- .config/qutebrowser/shortcuts.py
+++ .config/qutebrowser/shortcuts.py
@@ -11,6 +11,6 @@ config.bind(";sc", "set downloads.location.directory ~/Documents/Scripting ;; hi
 config.bind(";w", "set downloads.location.directory ~/Pictures/Wallpapers ;; hint links download")
 config.bind(";p", "set downloads.location.directory ~/Pictures ;; hint links download")
 config.bind(";s", "set downloads.location.directory ~/Pictures/PrivateScreenshots ;; hint links download")
-config.bind(";r", "set downloads.location.directory ~/Pictures/Random ;; hint links download")
+config.bind(";r", "set downloads.location.directory ~/Documents/Russian/ ;; hint links download")
 config.bind(";u", "set downloads.location.directory ~/Downloads ;; hint links download")
 config.bind(";c", "set downloads.location.directory ~/.config ;; hint links download")
