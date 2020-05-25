1d993c8e6e96fd4b4c50fbccb1224ed13563a929
Sofia B. Lopez <sofiabelenlopez00@gmail.com>
2020-05-25 12:42:31 +0300

    Change polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar top &
+polybar main -c $HOME/.config/polybar/config.ini &
