c3e2ad93dfddeb9ddb74710510f1c9fb8aa835c1
Sofia Lopez <sofiabelenlopez00@gmail.com>
2021-04-17 14:24:58 +0300

    Updating polybar configs

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -9,18 +9,18 @@ while true; do
     get_total_updates
 
     # notify user of updates
-    if hash notify-send &>/dev/null; then
-        if (( UPDATES > 50 )); then
-            notify-send -u critical -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
-        elif (( UPDATES > 25 )); then
-            notify-send -u normal -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
-        elif (( UPDATES > 2 )); then
-            notify-send -u low -i $NOTIFY_ICON \
-                "$UPDATES New packages"
-        fi
-    fi
+    # if hash notify-send &>/dev/null; then
+    #     if (( UPDATES > 50 )); then
+    #         notify-send -u critical -i $NOTIFY_ICON \
+    #             "You really need to update!!" "$UPDATES New packages"
+    #     elif (( UPDATES > 25 )); then
+    #         notify-send -u normal -i $NOTIFY_ICON \
+    #             "You should update soon" "$UPDATES New packages"
+    #     elif (( UPDATES > 2 )); then
+    #         notify-send -u low -i $NOTIFY_ICON \
+    #             "$UPDATES New packages"
+    #     fi
+    # fi
 
     # when there are updates available
     # every 10 seconds another check for updates is done
